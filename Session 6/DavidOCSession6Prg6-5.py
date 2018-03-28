52f5b613db319c6433eca7e00103a2a15accf08e
MacGriffon <oconnord82@gmail.com>
2018-03-28 14:46:21 -0500

    Updated project 6-5 to include additional options and offer them in a menu.

--- Session 6/DavidOCSession6Prg6-5.py
+++ Session 6/DavidOCSession6Prg6-5.py
@@ -7,31 +7,42 @@
 ################################################################################
 
 def main():
-    returnTotal()
-    returnAverage()
+    menuInput = 0
+    while menuInput !=3:
+        print('Select an option from the list below: \n1:Ruturn Total \n2:Return Average \
+            \n3:Exit')
+        menuInput = int(input('Input Selection: '))
+        if menuInput == 1:
+            returnTotal()
+        elif menuInput == 2:
+            returnAverage()
+        elif menuInput == 3:
+            break
+        else:
+            input('Invalid entry, please try again.')
 
 def readFileContents():
     count = 1
-    infile = open('z:/numbers.txt', 'r')
+    infile = open('c:/devstuff/numbers.txt', 'r')
     line = infile.readline()
     total = 0
     try:
-        while line !='\n' and line !=' \n':
+        while line !='\n' and line !=' \n' and line != '':
             print(str(count) + '\t' + format(int(line),',.1f') + '\n' ,end='')
             total += int(line)
             line = infile.readline()
             count += 1
         infile.close()
+        return total, (count-1)
     except Exception as err:
         print(err)
-    return total, count
 
 def returnTotal():
     (total, count) = readFileContents()
-    print('\nTotal:\t' + format(int(total),',.1f'))
+    print('\nTotal:\t' + format(int(total),',.1f') + '\n')
 
 def returnAverage():
     (total, count) = readFileContents()
-    print('\nAverage:\t' + format(int(total/count),',.1f'))
+    print('\nAverage:\t' + format(int(total/count),',.1f') + '\n')
 
 main()
