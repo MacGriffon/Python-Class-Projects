7728962147883d98bf76daaa05b71b088d43b20c
MacGriffon <oconnord82@gmail.com>
2018-03-27 21:12:29 -0500

    Upload additional files to review and updates to project 6-5

--- Session 6/DavidOCSession6Prg6-5.py
+++ Session 6/DavidOCSession6Prg6-5.py
@@ -3,16 +3,35 @@
 #
 #Author: David OConnor
 #Date: 3/25/2018
-#Purpose: Read a file and output the sum of the numbers contained within.
+#Purpose: Read a file and output the sum or average of the numbers contained within.
 ################################################################################
 
 def main():
+    returnTotal()
+    returnAverage()
+
+def readFileContents():
+    count = 1
     infile = open('z:/numbers.txt', 'r')
     line = infile.readline()
-    while line:
-        print(line,end='')
-        line = infile.readline()
-    infile.close()
-    # print(str(filesum))
-#Make things happen
+    total = 0
+    try:
+        while line !='\n' and line !=' \n':
+            print(str(count) + '\t' + format(int(line),',.1f') + '\n' ,end='')
+            total += int(line)
+            line = infile.readline()
+            count += 1
+        infile.close()
+    except Exception as err:
+        print(err)
+    return total, count
+
+def returnTotal():
+    (total, count) = readFileContents()
+    print('\nTotal:\t' + format(int(total),',.1f'))
+
+def returnAverage():
+    (total, count) = readFileContents()
+    print('\nAverage:\t' + format(int(total/count),',.1f'))
+
 main()
