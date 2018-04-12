760ec072a2085d25a25f99ee4fe9be63ddfed3ea
David OConnor <OCONDAVI@chrobinson.com>
2018-04-12 11:35:50 -0500

    Commit

--- Messing Around/DistanceCalc.py
+++ Messing Around/DistanceCalc.py
@@ -4,6 +4,9 @@
 #Purpose: 
 ################################################################################
 
+
+
+
 def main():
     (speed,time) = inputs()
     CalculationOuput(speed,time)
@@ -16,7 +19,7 @@ def inputs():
 def CalculationOuput(s,t):
     loop = 1
     for loop in range (1,t+1):
-        print('Distance traveled is ' + str(int(loop) * int(s)) + ' miles in ' + str(loop) + 'hour(s).')
+        print('Distance traveled is ' + str(int(loop) * int(s)) + ' miles in ' + str(loop) + ' hour(s).')
         loop += 1
 #Make things happen
 
---
+++ Session 7/DavidOCSession7Prg7-6.py
@@ -0,0 +1,34 @@
+################################################################################
+#FOP Session 7 Program 7-6
+#
+#Author: David OConnor
+#Date: 4/9/2018
+#Purpose: Display a list of numbers higher than a user input number.
+################################################################################
+
+
+
+def main():
+    menuInput = 0
+    while menuInput !=99:
+        NUMBER_INDEX = [1,2,3,4,5,6,7,8,9,10]
+        print('1:Enter an integer between 1 and 10 (or 99 to exit): ')
+        menuInput = int(input('Input Selection: '))
+        if menuInput >= 1 and menuInput <= 10:
+            displayList(NUMBER_INDEX)
+            listAboveSelection(NUMBER_INDEX,menuInput)
+        elif menuInput == 99:
+            break
+        else:
+            print('',end='')
+
+def displayList(NUMBER_INDEX):
+    x = 1
+    print(NUMBER_INDEX)
+
+def listAboveSelection(NUMBER_INDEX,menuInput):
+    x = menuInput
+    del NUMBER_INDEX[0:menuInput]
+    print(NUMBER_INDEX)
+
+main()
