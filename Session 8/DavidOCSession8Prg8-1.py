d2695f4f6f8928e14cac183d142fb1632d48f31c
David OConnor <OCONDAVI@chrobinson.com>
2018-04-12 15:36:47 -0500

    Commit

--- Session 8/DavidOCSession8Prg8-1.py
+++ Session 8/DavidOCSession8Prg8-1.py
@@ -3,15 +3,21 @@
 #
 #Author: David OConnor
 #Date: 4/10/2018
-#Purpose: 
+#Purpose: Accept a full name, break it apart, display with suggested username.
 ################################################################################
 
 def main():
+    full_name, first, middle, last = nameInput()
+    usernameOutput(full_name, first, middle, last)
 
+def nameInput():
     full_name = input('What is your full name? ')
     first, middle, last = full_name.split()
-    print('You entered ', full_name)
 
+    return full_name, first, middle, last
+
+def usernameOutput(full_name, first, middle, last):
+    print('You entered ', full_name)
     print('UserId should be', first.lower()[0] + middle.lower()[0] + last.lower()[0:6])
 
 main()
--- Session 8/DavidOCSession8Prg8-3.py
+++ Session 8/DavidOCSession8Prg8-3.py
@@ -3,20 +3,25 @@
 #
 #Author: David OConnor
 #Date: 4/10/2018
-#Purpose: 
+#Purpose: Accept a date and output that date in a formatted way.
 ################################################################################
 
 def main():
-    MONTHS = ('January', 'February', 'March', 'April', 'May',\
-             'June', 'July', 'August', 'September', 'October',\
-             'November', 'December')
+    dateEntered=dateInput()
+    dateOutput(dateEntered)
 
-    date_string = input('Enter a date in the format mm/dd/yyyy: ')
-    
+def dateInput():
+    dateEntered = input('Enter a date in the format mm/dd/yyyy: ')
+    return dateEntered
+
+def dateOutput(date_string):    
+    MONTHS = ('January', 'February', 'March', 'April', 'May',\
+    'June', 'July', 'August', 'September', 'October',\
+    'November', 'December')
+
     month_str, day_str, year_str = date_string.split('/')
     month = int(month_str)
 
     print(MONTHS[month - 1], day_str + ',', year_str + '.')
 
 main()
-
