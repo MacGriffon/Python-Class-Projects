930ab5efcafc07c581318bca3d9c7fb68987b730
MacGriffon <oconnord82@gmail.com>
2018-04-19 18:05:48 -0500

    Commit

--- Session 9/DavidOCSession9Prg9.py
+++ Session 9/DavidOCSession9Prg9.py
@@ -5,6 +5,38 @@
 #Purpose: 
 ################################################################################
 
-def main():
 
+def main():
+    menuInput = 0
+    while menuInput == 0:
+        print('Make a selection:\n1: Enter a Class Room Number. \
+            \n2: Enter a Class Instructor.\n3: Enter a Class Meeting Time.\
+            \n4: Exit.')
+        menuInput=int(input('Selection: '))
+        if menuInput == 1:
+            enterClassroom()
+            menuInput = 0
+        elif menuInput == 2:
+            enterInstructor()
+            menuInput = 0
+        elif menuInput == 3:
+            enterTime()
+            menuInput = 0
+        elif menuInput == 4:
+            applicationExit()
+        else:
+            menuInput = 0
+
+def enterClassroom():
+    print('option 1')
+    
+def enterInstructor():
+    print('option 2')
+    
+def enterTime():
+    print('option 3')
+    
+def applicationExit():
+    SystemExit
+    
 main()
